65416217c9834ab6f9eed9145c9fbaa9999c5689
matias lopez <matii.94_19@yahoo.com.ar>
2025-04-15 10:57:12 -0300

    quintocommit

--- matias_coderhouse/matias_aplicacion/urls.py
+++ matias_coderhouse/matias_aplicacion/urls.py
@@ -1,11 +1,13 @@
-
-  
 from django.urls import path
 from . import views
+from .views import CursoUpdateView
 
 
 urlpatterns = [
-    path('', views.listado, name='inicio'),  # Esta es la raíz del sitio
-    path('listado/', views.listado, name='listado'),
-    path('crear-curso/', views.crear_curso, name='crear_curso'),
+    path('', views.listado_cursos, name='inicio'),
+    path('listado/', views.listado_cursos, name='listado_cursos'),
+    path('crear/', views.crear_curso, name='crear_curso'),
+    path('curso/<int:pk>/', views.CursoDetalleView.as_view(), name='detalle_curso'),
+    path('curso/editar/<int:pk>/', CursoUpdateView.as_view(), name='editar_curso'),
+
 ]
--- matias_coderhouse/matias_aplicacion/views.py
+++ matias_coderhouse/matias_aplicacion/views.py
@@ -1,23 +1,35 @@
 from django.shortcuts import render, redirect
+from .models import Curso
 from .forms import CursoForm
-from .models import Curso  # importo el modelo
+from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
 
-# mostrar todos los cursos
-def listado(request):
-    cursos = Curso.objects.all()
-    return render(request, 'matias_aplicacion/listado.html', {'cursos': cursos})
 
-# página de inicio
-def inicio(request):
-    return render(request, 'matias_aplicacion/inicio1.html')
-
-# crear un curso
 def crear_curso(request):
     if request.method == 'POST':
         form = CursoForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('listado')
+            return redirect('listado_cursos')
     else:
         form = CursoForm()
     return render(request, 'matias_aplicacion/crear_curso.html', {'form': form})
+
+def listado_cursos(request):
+    cursos = Curso.objects.all()
+    return render(request, 'matias_aplicacion/listado.html', {'cursos': cursos})
+
+class CursoDetalleView(DetailView):
+    model = Curso
+    template_name = 'matias_aplicacion/index.html' 
+
+
+from django.views.generic.edit import UpdateView
+from .models import Curso
+
+class CursoUpdateView(UpdateView):
+    model = Curso
+    fields = ['nombre', 'puntos']  
+    template_name = 'matias_aplicacion/editar_curso.html'
+    success_url = '/listado/'  
